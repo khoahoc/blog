9d3ecfbf80339ec52c1c12993e86f85aaefd3253
unknown <Khoa Hoc>
2021-03-01 09:59:48 +0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,7 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "khoahoc", // Usually your GitHub org/user name.
   projectName: "khoahoc.github.io", // Usually your repo name.
+  cname: "sudosys.com",
   themeConfig: {
     navbar: {
       title: "SudoSys.com",
